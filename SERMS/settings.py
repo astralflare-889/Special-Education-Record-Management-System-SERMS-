7f256a353c6202e6593ff3ef0f7e32fe3f79db42
astralflare-889 <dawiehrankin@gmail.com>
2025-10-24 02:50:12 +0000

    Add Vercel deployment configuration

--- SERMS/settings.py
+++ SERMS/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,4 +139,4 @@ import os
 ALLOWED_HOSTS = ['special-education-record-management.vercel.app', 'localhost', '127.0.0.1', '.vercel.app', 'testserver']
 
 # For production
-DEBUG = True
+DEBUG = False
